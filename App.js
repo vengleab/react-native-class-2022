9007d364758dcf851c241ee1728ad91b91b53371
SO Vengleab <vengleab@slash.co>
2022-09-15 18:35:31 +0700

    Week6 Integrate with api

--- App.js
+++ App.js
@@ -1,33 +1,77 @@
 import React from 'react';
-import {View, ScrollView, TouchableOpacity} from 'react-native';
+import {View, ScrollView, TouchableOpacity, FlatList, Text} from 'react-native';
 import Product from './Product';
 import {products} from './products.json';
 
-const Products = () => {
+const Products = props => {
+  const {products} = props;
   return (
-    <ScrollView>
-      {products.map(product => {
-        console.log('production =====>', product);
+    // <ScrollView>
+    //   {products.map(product => {
+    //     console.log('production =====>', product);
 
+    //     return (
+    //       <Product
+    //         key={product.id}
+    //         id={product.id}
+    //         title={product.title}
+    //         description={product.description}
+    //         image={product.thumbnail}
+    //       />
+    //     );
+    //   })}
+    // </ScrollView>
+    <FlatList
+      data={products}
+      renderItem={prop => {
+        // console.log('======>', JSON.stringify(prop));
+        const {item: product} = prop;
         return (
           <Product
             key={product.id}
-            id={product.id}
             title={product.title}
             description={product.description}
             image={product.thumbnail}
           />
         );
-      })}
-    </ScrollView>
+      }}
+    />
   );
 };
+
+class ProductsWrapper extends React.Component {
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    console.log('On load');
+    // fetch('https://dummyjson.com/products')
+    //   .then(result => {
+    //     return result.json();
+    //   })
+    //   .then(jsonData => {
+    //     console.log('Data', jsonData);
+    //     this.setState({data: jsonData.products});
+    //   });
+
+    const result = await fetch('https://dummyjson.com/products');
+    const jsonData = await result.json();
+    this.setState({data: jsonData.products});
+  }
+
+  render() {
+    console.log('On render');
+    return <Products products={this.state.data} />;
+  }
+}
 
 export default () => {
   console.log(products);
   return (
     <View>
-      <Products />
+      {/* <Products /> */}
+      <ProductsWrapper />
     </View>
   );
 };
