c3a9d110cdc2682ae499521d498fd70041bcbc88
SO Vengleab <vengleab@slash.co>
2022-09-15 20:28:13 +0700

    Week7 Add Ui kits

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/screen/Week7Home';
 import {name as appName} from './app.json';
+// import {NativeBaseProvider} from 'native-base';
+
+// const MyApp = () => {
+//   return (
+//     // <NativeBaseProvider>
+//       <App />
+//     // </NativeBaseProvider>
+//   );
+// };
 
 AppRegistry.registerComponent(appName, () => App);
