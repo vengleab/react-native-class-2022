6ab820a80af74da7e6ccbb9305c0282ba4ec51d2
SO Vengleab <vengleab@slash.co>
2022-09-22 18:58:44 +0700

    Week08 Configure 3rd icon with NativeBase

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/screen/Week7Home';
 import {name as appName} from './app.json';
-// import {NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 
-// const MyApp = () => {
-//   return (
-//     // <NativeBaseProvider>
-//       <App />
-//     // </NativeBaseProvider>
-//   );
-// };
+const MyApp = () => {
+  return (
+    <NativeBaseProvider>
+      <App />
+    </NativeBaseProvider>
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MyApp);
--- src/screen/Week7Home.js
+++ src/screen/Week7Home.js
@@ -1,49 +1,40 @@
-import {Box, Center, Checkbox, CheckIcon, NativeBaseProvider, Select} from 'native-base';
+import {
+  Box,
+  Center,
+  Checkbox,
+  CheckIcon,
+  NativeBaseProvider,
+  PlayIcon,
+  AddIcon,
+  Select,
+  HStack,
+} from 'native-base';
 import React from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
 import Button from '../common/Button';
 import {Icon} from 'native-base';
-import {Ionicons, FontAwesome} from '@expo/vector-icons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default () => {
-  const [service, setService] = React.useState('');
   return (
-    <NativeBaseProvider>
-      <SafeAreaView>
-        {/* <Button onPress={() => alert('Click me')} title="Click me" />
-      <Button onPress={() => alert('Press me')} title="Press me" /> */}
-        <Button onClick={() => alert('Click')} color="aqua">
-          Click me
-        </Button>
-        <Button>Press me</Button>
-        <Checkbox value="one" my={2}>
-          UX Research
-        </Checkbox>
-        <Icon name="home" />
-        <Icon type="FontAwesome" name="home" />
-        <Text>Text</Text>
-        <Center>
-          <Box maxW="300">
-            <Select
-              selectedValue={service}
-              minWidth="200"
-              accessibilityLabel="Choose Service"
-              placeholder="Choose Service"
-              _selectedItem={{
-                bg: 'teal.600',
-                endIcon: <CheckIcon size="5" />,
-              }}
-              mt={1}
-              onValueChange={itemValue => setService(itemValue)}>
-              <Select.Item label="UX Research" value="ux" />
-              <Select.Item label="Web Development" value="web" />
-              <Select.Item label="Cross Platform Development" value="cross" />
-              <Select.Item label="UI Designing" value="ui" />
-              <Select.Item label="Backend Development" value="backend" />
-            </Select>
-          </Box>
-        </Center>
-      </SafeAreaView>
-    </NativeBaseProvider>
+    <SafeAreaView>
+      <HStack>
+        <AddIcon />
+        <PlayIcon />
+        <Icon
+          as={AntDesign}
+          name="android1"
+          color="coolGray.800"
+          _dark={{
+            color: 'blue',
+          }}
+          size={28}
+        />
+        <Icon as={Entypo} name="app-store" size={28} />
+        <Icon as={MaterialCommunityIcons} name="account-arrow-left" size={28} />
+      </HStack>
+    </SafeAreaView>
   );
 };
